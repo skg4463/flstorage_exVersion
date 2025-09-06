c19147d39007dbf385c5cb968abe061c0ab6a8eb
raco0n <skg4463@gmail.com>
2025-09-07 04:57:40 +0900

    2.1 error 추가

--- x/storage/types/errors.go
+++ x/storage/types/errors.go
@@ -9,4 +9,7 @@ import (
 // x/storage module sentinel errors
 var (
 	ErrInvalidSigner = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	// --- 아래 두 줄 추가 ---
+	ErrInvalidOriginalHash = errors.Register(ModuleName, 1101, "invalid original hash")
+	ErrFileAlreadyExists   = errors.Register(ModuleName, 1102, "file with this hash already exists")
 )
